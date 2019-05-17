154ca5c9dc68fa3d8365146584b2f9f42e8d19d6
juheon97 <son16007@byui.edu>
2019-05-17 17:01:45 -0600

    I added the second line

--- src/Cheney/HelloFromJoe.java
+++ src/Cheney/HelloFromJoe.java
@@ -3,5 +3,6 @@ package Cheney;
 public class HelloFromJoe {
     public static void main(String[] args) {
         System.out.print( "Hello, my name is Joe Cheney" );
+        System.out.print("Hello,  Joe Cheney, my name is Juheon Song");
     }
 }
